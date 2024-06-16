b6072e998d94a5e54e6a80b20a3b12e2b3e822e5
erimendes <eri.mendes32@gmial.com>
2024-06-16 19:34:34 +0100

    Atualizaões no backend

--- frontend/src/components/ui/Button/index.tsx
+++ frontend/src/components/ui/Button/index.tsx
@@ -4,24 +4,24 @@ import styles from './styles.module.scss';
 import { FaSpinner } from 'react-icons/fa'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    loading?: boolean,
-    children: ReactNode,
+  loading?: boolean,
+  children: ReactNode,
 }
 
 export function Button({ loading, children, ...rest }: ButtonProps){
-    return(
-        <button 
-            className={styles.button}
-            disabled={loading}
-            {...rest}
-        >
-            { loading ? (
-                <FaSpinner color="#FFF" size={16} />
-            ) : (
-                <a className={styles.button}>
-                    {children}
-                </a>
-            )}
-        </button>
-    )
+  return(
+    <button 
+    className={styles.button}
+    disabled={loading}
+    {...rest}
+    >
+      { loading ? (
+        <FaSpinner color="#FFF" size={16} />
+      ) : (
+        <a className={styles.buttonText}>
+          {children}
+        </a>
+      )}
+    </button>
+  )
 }
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,45 +1,45 @@
-import { useContext, FormEvent, useState } from "react";
+import { useState, FormEvent, useContext } from "react";
 
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../../styles/home.module.scss";
+import styles from "../../../styles/home.module.scss";
 
-import logoImg from "../../public/logo.svg";
+import logoImg from "../../../public/logo.svg";
 
-import { Input } from "../components/ui/Input";
-import { Button } from "../components/ui/Button";
+import { Input } from "../../components/ui/Input";
+import { Button } from "../../components/ui/Button";
+
+import { AuthContext } from "../../contexts/AuthContext";
 import { toast } from "react-toastify";
-
-import { AuthContext } from "../contexts/AuthContext";
 
 import Link from "next/link";
 
-import { canSSRGuest } from "../utils/canSSRGuest";
+export default function SignUp() {
+  const { signUp } = useContext(AuthContext);
 
-export default function Home() {
-  const { signIn } = useContext(AuthContext);
-
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const [loading, setLoading] = useState(false);
 
-  async function handleLogin(event: FormEvent) {
+  async function handleSignUp(event: FormEvent) {
     event.preventDefault();
 
-    if (email === "" || password === "") {
-      toast.error("Preencha os campos");
+    if (name === "" || email === "" || password === "") {
+      toast.error("Preencha todos os campos");
       return;
     }
 
     setLoading(true);
 
     let data = {
+      name,
       email,
       password,
     };
 
-    await signIn(data);
+    await signUp(data);
 
     setLoading(false);
   }
@@ -47,13 +47,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
-          <form onSubmit={handleLogin}>
+          <h1>Criando sua conta</h1>
+
+          <form onSubmit={handleSignUp}>
+            <Input
+              placeholder="Digite seu nome"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
             <Input
               placeholder="Digite seu email"
               type="text"
@@ -69,21 +78,15 @@ export default function Home() {
             />
 
             <Button type="submit" loading={loading}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link className={styles.text} href="/signup">
-            Nao possui uma conta? Cadastre-se
+          <Link className={styles.text} href="/">
+            Já possui uma conta? Faça login!
           </Link>
         </div>
       </div>
     </>
   );
 }
-
-export const getServerSideProps = canSSRGuest(async (ctx) => {
-  return {
-    props: {},
-  };
-});
