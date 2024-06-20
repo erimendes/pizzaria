1cdd16824545be8a13ab8128192d8dbebb961bcd
erimendes <eri.mendes32@gmial.com>
2024-06-20 03:09:38 +0100

    Create function AppRoutes

--- tarefas/App.js
+++ tarefas/App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar backgroundColor="#1d1d2e" barStyle="light-content" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
