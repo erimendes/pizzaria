260c411b88d24e30dd3e678a6fc8a4341c4c9478
unknown <Administrador@UF074492.ad.ufabc.int.br>
2024-06-21 09:06:49 -0300

    update

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -84,7 +84,7 @@ export function AuthProvider({children}: AuthProviderProps){
         email,
         password
       })
-      //console.log(response.data);
+      console.log(response.data);
 
       const { id, name, token } = response.data;
 
